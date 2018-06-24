2aa5b0bd18ee63f9ae8ff31c6c848562cd806f64
chan <chengdong.sina@163.com>
2018-06-25 00:00:10 +0800

    first commit

---
+++ src/main/java/com/cd/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.cd.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping("/hi")
+    public String hi (String name) {
+        return helloService.hiService(name);
+    }
+}
